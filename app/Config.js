5a2224127d3a9fbfd61d91a831b37929489b693f
IsmatAyash <ismat.ayash@gmail.com>
2022-07-07 10:39:50 +0300

    updated api url

--- app/Config.js
+++ app/Config.js
@@ -1,5 +1,5 @@
 // export const API_URL = 'https://expo-stripe-server-example.glitch.me';
-export const API_URL = 'http://localhost:5001/api';
-// export const API_URL = 'https://beirut-life-server.herokuapp.com/api';
+// export const API_URL = 'http://localhost:5001/api';
+export const API_URL = 'https://beirut-life-server.herokuapp.com/api';
 export const STRIPE_PUBLISHABLE_KEY =
   'pk_test_51LI97YLb9RE2ovqgVKOqzRa3UndrkaYyZUhAwQE26FXG33cfaBhisWJTla1XLXSCUI16b9gRyDpbCiAMrviDswFN00RL4kOXXj';
