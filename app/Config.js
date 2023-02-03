b6bd42bdb2504407d95b88536a1b8070a67a514a
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2023-02-03 11:09:33 +0200

    adding disabled recurring button to checkoutScreen

--- app/Config.js
+++ app/Config.js
@@ -1,6 +1,8 @@
 // export const API_URL = 'http://localhost:5001/api';
-export const API_URL = 'https://beirut-life-server.herokuapp.com/api';
+export const API_URL = 'http://10.99.81.104:5001/api';
+// export const API_URL = 'https://beirut-life-server.herokuapp.com/api';
 export const STRIPE_PUBLISHABLE_KEY =
   'pk_test_51LI97YLb9RE2ovqgVKOqzRa3UndrkaYyZUhAwQE26FXG33cfaBhisWJTla1XLXSCUI16b9gRyDpbCiAMrviDswFN00RL4kOXXj';
 export const BBC_PAY_URL = 'https://beirutlife-pay.netlify.app/';
-export const BBC_PAY_URL_LOCAL = 'http://localhost:3000/';
+// export const BBC_PAY_URL_LOCAL = 'http://localhost:3000/';
+export const BBC_PAY_URL_LOCAL = 'http://10.99.81.104:3000/';
